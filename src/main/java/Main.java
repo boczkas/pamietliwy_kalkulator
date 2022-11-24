f3890c6f525be6fde4a7d978547b49de54944043
Przemek <przemyslawjakubowski@gmail.com>
2022-11-24 14:24:57 +0100

    Zadanie kalkulator

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+//    Napisz kalkulator, który:
+
+//    1. Będzie obsługiwał następujące działania:
+//      - dodawanie
+//      - odejmowanie
+//      - mnożenie
+//      - dzielenie
+//    2. Będzie działał bez przerwy
+//    3. W dowolnym momencie będzie można zakończyć jego działanie przez wpisanie "quit"
+//    4. Będzie posiadał moduł pamięci, który będzie podawał wynik danych obliczeń z pamięci,
+//          jeśli dana operacja była już wykonywana
+
+//    Zadanie domowe:
+//    1. Dodaj funkcjonalność liczenia pierwiastka (tylko dla liczb dodatnich)
+//    2. Dodaj funkcjonalność wyświetlania znajdujących się w pamięci obliczeń
+
+
+//    Trzy fazy patrzenia jak ktoś kodzi:
+//    1. Sam bym nigdy na to nie wpadł/wpadła
+//    2. Aha, tak to można zrobić
+//    3. Ja bym to raczej zrobił/zrobiła inaczej
+}
